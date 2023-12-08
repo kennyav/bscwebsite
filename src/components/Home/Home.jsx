3bef0321cacb0a118c70bc173426c7bbff474f1c
Koz <kenneth.averna@gmail.com>
2023-12-07 20:57:01 -0800

    fixed the buttons and added the mission statement

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,11 @@
 import React from 'react'
 import './style.css'
 
+// components
+import MissionStatement from '../Mission/MissionStatement';
+
 
 export default function Home() {
-  // const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
-
-  // useEffect(() => {
-  //   const handler = (e) => {
-  //     setMousePos({ x: e.clientX, y: e.clientY });
-  //   };
-  //   window.addEventListener('mousemove', handler);
-
-  //   return () => {
-  //     window.removeEventListener('mousemove', handler)
-  //   }
-  // })
-
   const containerStyle = {
     width: '100vw',
     height: '100vh',
@@ -45,9 +35,13 @@ export default function Home() {
 
   return (
     <>
-      {/* <MultiLayerParallax imageFull={`url(/image-full.png)`} imageTop={`url(/image-top.png)`} height={100} title={"Empowering Dreams, Crafting Solutions: Your Vision, Our Code."} font={'60px'} /> */}
       <div style={containerStyle}>
         <p className='title'>Empowering Dreams, Crafting Solutions: Your Vision, Our Code.</p>
+        <div className='cta-buttons'>
+          <button class="button-68">Get In Touch</button>
+          <button class="button-69">Work</button>
+          <button class="button-69">Agency</button>
+        </div>
         <div style={overlayStyle} />
         <img
           src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/9a995e53313911.592fd4ffb4745.gif"
@@ -55,17 +49,7 @@ export default function Home() {
           alt='Home Page Gif'
         ></img>
       </div>
-      <div className='cta-buttons'>
-        <button class="button-68">Get In Touch</button>
-        <button class="button-68">Work</button>
-        <button class="button-68">Agency</button>
-      </div>
-      {/* <h1 className='title'>BlockSmith Consulting, LLC</h1>
-      <div className='dot-container'>
-        {Array.from({ length: 240 }, (_, i) => (
-          <Dot key={i} mousePos={mousePos}></Dot>
-        ))}
-      </div> */}
+      <MissionStatement />
     </>
   )
 }
---
+++ src/components/Mission/MissionStatement.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+export default function MissionStatement() {
+   return (
+      <div className='missionContainer'>
+         <p className='missionStatement'>
+            We're a web consulting business partnering with you to understand your vision and challenges, providing end-to-end services for web applications and sites through innovative and user-centric software solutions
+         </p>
+
+      </div>
+   )
+}
